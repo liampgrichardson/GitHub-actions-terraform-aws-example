d0c70d88a39590b43a8b4b5f3cae1e120ebe173d
liamrichardson <liampgrichardson@gmail.com>
2024-12-04 17:35:46 +0000

    moded region and terrafrom yml

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "s306022024"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
+    region         = "eu-west-1"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "aws_region" {
   description = "AWS region"
-  default     = "us-east-1"
+  default     = "eu-west-1"
 }
 
 variable "instance_type" {
